18e2695a2bd0c4a47f0a6df8806c3cd021fcc47e
jcasas <jcasas@kooomo.com>
2024-06-05 19:51:57 +0200

    added vector

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 - `git clone https://github.com/zgcasas/colorbookremovebackground.git`
 - `npm install`
 - Put the images on `input`
-- `node convert.js`
+- `node convert.js` to remove the background
 - Get the images on `output`
+- `node vector.js`
+- Get the vectorize images on `output_svg`
---
+++ vector.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+const potrace = require('potrace');
+const { promisify } = require('util');
+
+const inputDir = 'output';
+const outputDir = 'output_svg';
+
+// Promisify the potrace functions
+const trace = promisify(potrace.trace);
+
+// Ensure input and output directories exist
+if (!fs.existsSync(inputDir)) {
+    fs.mkdirSync(inputDir);
+}
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+}
+
+// Function to process each image file
+const processImage = async (inputFilePath, outputFilePath) => {
+    try {
+        const svg = await trace(inputFilePath, { color: 'black' });
+        fs.writeFileSync(outputFilePath, svg);
+        console.log(`Processed: ${inputFilePath}`);
+    } catch (err) {
+        console.error(`Error processing image: ${inputFilePath}`, err);
+    }
+};
+
+// Read all PNG files from the input directory
+const imageExtension = '.png';
+fs.readdir(inputDir, (err, files) => {
+    if (err) {
+        console.error('Error reading input directory:', err);
+        return;
+    }
+
+    files.forEach(file => {
+        const ext = path.extname(file).toLowerCase();
+        if (ext === imageExtension) {
+            const inputFilePath = path.join(inputDir, file);
+            const outputFilePath = path.join(outputDir, `${path.parse(file).name}.svg`);
+            processImage(inputFilePath, outputFilePath);
+        }
+    });
+});
